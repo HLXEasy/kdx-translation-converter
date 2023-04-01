f0b9859df158aa8402bbfb83ca2a8f551ffd026f
HLXEasy <hlxeasy@gmail.com>
2023-04-01 23:26:45 +0200

    Added initial content

---
+++ src/main/java/org/kaspa/kdx/translationconverter/TranslationConverterApplication.java
@@ -0,0 +1,13 @@
+package org.kaspa.kdx.translationconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslationConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranslationConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/kaspa/kdx/translationconverter/TranslationConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kaspa.kdx.translationconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslationConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
