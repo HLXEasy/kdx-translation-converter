e28aeea76d4a917db422cba8ee4958cbe672dd78
HLXEasy <hlxeasy@gmail.com>
2023-04-02 23:23:50 +0200

    Added class skeletons for upcoming split and merge functionality

---
+++ src/main/java/org/kaspa/kdx/translationconverter/LangFileMerger.java
@@ -0,0 +1,7 @@
+package org.kaspa.kdx.translationconverter;
+
+public class LangFileMerger {
+    public void doMerge() {
+        System.out.println("Merge: to be implemented");
+    }
+}
---
+++ src/main/java/org/kaspa/kdx/translationconverter/LangFileSplitter.java
@@ -0,0 +1,7 @@
+package org.kaspa.kdx.translationconverter;
+
+public class LangFileSplitter {
+    public void doSplit() {
+        System.out.println("Split: to be implemented");
+    }
+}
